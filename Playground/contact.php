387a93e73d7a0501bd757817cfa492584acf4821
unknown <Brian@.(none)>
2013-09-09 23:25:27 -0400

    other changes

--- Playground/contact.php
+++ Playground/contact.php
@@ -4,11 +4,18 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<!-- Bootstrap -->
 		<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" media="screen">
+		<style type="text/css">
+			body {
+				padding-top: 70px;
+			}
+		</style>
 	</head>
-	<body>
+	<body { padding-top: 70px; }>
 		<?
 		include "nav.php";
 		?>
-		<p>This is the contact page</p>
+		<p>
+			This is the contact page
+		</p>
 	</body>
-	</html>
+</html>
--- Playground/index.php
+++ Playground/index.php
@@ -5,7 +5,11 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<!-- Bootstrap -->
 		<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" media="screen">
-
+		<style type="text/css">
+			body {
+				padding-top: 70px;
+			}
+		</style>
 		<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
 		<!--[if lt IE 9]>
 		<script src="../../assets/js/html5shiv.js"></script>
@@ -13,6 +17,7 @@
 		<![endif]-->
 	</head>
 	<body>
+		
 		<?
 		include "nav.php";
 		?>
