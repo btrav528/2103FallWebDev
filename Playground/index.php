40644315df3dd7d9db1420ccdc7264e2afee813e
unknown <n02207313@.active.newpaltz.edu>
2013-09-12 15:45:18 -0400

    Fixed dropdown menu issue

--- Playground/index.php
+++ Playground/index.php
@@ -61,7 +61,6 @@
 		</div>
 		</div>
 
-		<script src="//code.jquery.com/jquery.js"></script>
-		<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+
 	</body>
 </html>
